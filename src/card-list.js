deb8df0a51118052854e1cdd7dc3da5885d4c180
AaronLobitana <faaron.lobitana@gmail.com>
2023-02-28 15:51:15 -0500

    card-list
    added aaron's cards

--- src/card-list.js
+++ src/card-list.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import "mini-resume/src/mini-resume.js";
 
 const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
@@ -8,46 +9,14 @@ class CardList extends LitElement {
   }
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
+    
+    .aaronCard{
+      display: inline-block;
+      width: 100%;
       text-align: center;
-      background-color: var(--card-list-background-color);
     }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
+    
+    
   `;
 
   constructor() {
@@ -57,30 +26,147 @@ class CardList extends LitElement {
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
+      <div class="aaronCard">
+        <mini-resume memeTop="me: doesn't have a professional headshot" memeBottom="also me: " new-color="black" shadow-enable>
+          <div slot="infoslot">
+            <ul>
+              <details class="skills">
+                <summary>Skills</summary>
+                <li>Placeholder skills</li>
+              </details>
 
-        <p>Edit <code>src/CardList.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+              <details class="exp">
+                <summary>Work Experience</summary>
+                <li>Placeholder work experince</li>
+              </details>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+              <details class="course">
+                <summary>Relevant Course Work</summary>
+                <li>Placeholder relevant coursework</li>
+              </details>
+            </ul>
+          </div>
+          <div slot="college">
+            <h4>Pennsylvania State University</h4>
+          </div>
+          <div slot="major">
+            <h4>B.S. Cybersecurity</h4>
+          </div>
+        </mini-resume>
+        
+        <mini-resume memeTop="me: still doesn't have a professional headshot" shadow-enable memeBottom="still me:" name="Matthew Guiddy" new-color="green" imageURL="https://st.depositphotos.com/1269204/1219/i/600/depositphotos_12196477-stock-photo-smiling-men-isolated-on-the.jpg">
+          <div slot="infoslot">
+            <ul>
+              <details class="skills">
+                <summary>Skills</summary>
+                <li>Placeholder skills</li>
+              </details>
+
+              <details class="exp">
+                <summary>Work Experience</summary>
+                <li>Placeholder work experince</li>
+              </details>
+
+              <details class="course">
+                <summary>Relevant Course Work</summary>
+                <li>Placeholder relevant coursework</li>
+              </details>
+            </ul>
+          </div>
+          <div slot="college">
+            <h4>University of Pittsburgh</h4>
+          </div>
+          <div slot="major">
+            <h4>B.S. Computer Science</h4>
+          </div>
+        </mini-resume>
+
+        <mini-resume memeTop="blah" shadow-enable memeBottom="bleh" name="Brim Reaper" new-color="blue" imageURL="https://pbs.twimg.com/media/FajpQj_XwAEwCWw.jpg">
+          <div slot="infoslot">
+            <ul>
+              <details class="skills">
+                <summary>Skills</summary>
+                <li>Placeholder skills</li>
+              </details>
+
+              <details class="exp">
+                <summary>Work Experience</summary>
+                <li>Placeholder work experince</li>
+              </details>
+
+              <details class="course">
+                <summary>Relevant Course Work</summary>
+                <li>Placeholder relevant coursework</li>
+              </details>
+            </ul>
+          </div>
+          <div slot="college">
+            <h4>Colgate University</h4>
+          </div>
+          <div slot="major">
+            <h4>Fear The Reaper</h4>
+          </div>
+        </mini-resume>
+
+        <mini-resume memeTop="me: is a valedictorian and goes to top school" shadow-enable memeBottom="also me: gender studies yaes" name="Finger" new-color="pink" imageURL="https://i.scdn.co/image/ab67616d0000b2730b6deb6b039d4481823b3c8d">
+          <div slot="infoslot">
+            <ul>
+              <details class="skills">
+                <summary>Skills</summary>
+                <li>Placeholder skills</li>
+              </details>
+
+              <details class="exp">
+                <summary>Work Experience</summary>
+                <li>Placeholder work experince</li>
+              </details>
+
+              <details class="course">
+                <summary>Relevant Course Work</summary>
+                <li>Placeholder relevant coursework</li>
+              </details>
+            </ul>
+          </div>
+          <div slot="college">
+            <h4>John's Hopkins University</h4>
+          </div>
+          <div slot="major">
+            <h4>B.S. Gender Studies</h4>
+          </div>
+        </mini-resume>
+
+        <mini-resume memeTop="Live, Laugh" memeBottom="Love" shadow-enable name="Donald Cornbread" imageURL="https://pyxis.nymag.com/v1/imgs/b25/753/ef0495a30eac613c74c5db75f6036ba7fc-14-the-room-tommy-wiseau.rsquare.w700.jpg">
+          <div slot="infoslot">
+            <ul>
+              <details class="skills">
+                <summary>Skills</summary>
+                <li>Placeholder skills</li>
+              </details>
+
+              <details class="exp">
+                <summary>Work Experience</summary>
+                <li>Placeholder work experince</li>
+              </details>
+
+              <details class="course">
+                <summary>Relevant Course Work</summary>
+                <li>Placeholder relevant coursework</li>
+              </details>
+            </ul>
+          </div>
+          <div slot="college">
+            <h4>Louisiana State University</h4>
+          </div>
+          <div slot="major">
+            <h4>B.S. Gymnastic Arts</h4>
+          </div>
+        </mini-resume>
+        
+      </div>
+      
+
+      
+        
     `;
   }
 }
